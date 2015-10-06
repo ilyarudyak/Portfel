959f03e38e8e2738e4515c683ba8b8ebd88b53c2
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-10-06 23:29:14 +0300

    add db helper

--- app/src/main/java/com/ilyarudyak/android/portfel/data/PortfolioDbHelper.java
+++ app/src/main/java/com/ilyarudyak/android/portfel/data/PortfolioDbHelper.java
@@ -1,7 +1,47 @@
 package com.ilyarudyak.android.portfel.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by ilyarudyak on 9/24/15.
  */
-public class PortfolioDbHelper {
+public class PortfolioDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "portfolio.db";
+
+    public PortfolioDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_STOCK_TABLE = "CREATE TABLE " + PortfolioContract.StockTable.TABLE_NAME + " (" +
+                PortfolioContract.StockTable._ID +              " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                PortfolioContract.StockTable.SYMBOL +           " INTEGER UNIQUE NOT NULL, " +
+                PortfolioContract.StockTable.NAME +             " TEXT NOT NULL, " +
+                PortfolioContract.StockTable.CURRENCY +         " TEXT NOT NULL, " +
+                PortfolioContract.StockTable.STOCK_EXCHANGE +   " TEXT NOT NULL " +
+                " );";
+
+        final String SQL_CREATE_STOCK_QUOTE_TABLE = "CREATE TABLE " + PortfolioContract.StockQuoteTable.TABLE_NAME + " (" +
+                PortfolioContract.StockQuoteTable._ID +         " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                PortfolioContract.StockQuoteTable.STOCK_ID +    " INTEGER NOT NULL, " +
+                PortfolioContract.StockQuoteTable.PRICE +       " TEXT NOT NULL " +
+                " );";
+
+        db.execSQL(SQL_CREATE_STOCK_TABLE);
+        db.execSQL(SQL_CREATE_STOCK_QUOTE_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + PortfolioContract.StockTable.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PortfolioContract.StockQuoteTable.TABLE_NAME);
+        onCreate(db);
+    }
 }
