f6686da89b8feb6de2ef702989a1c5d2d224d77f
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-10-06 23:30:36 +0300

    add data utils

---
+++ app/src/main/java/com/ilyarudyak/android/portfel/utils/DataUtils.java
@@ -0,0 +1,51 @@
+package com.ilyarudyak.android.portfel.utils;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import com.ilyarudyak.android.portfel.data.PortfolioContract;
+
+import java.math.BigDecimal;
+
+import yahoofinance.Stock;
+import yahoofinance.quotes.stock.StockQuote;
+
+/**
+ * Created by ilyarudyak on 10/6/15.
+ */
+public class DataUtils {
+
+    public static ContentValues buildContentValues(Stock s) {
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(PortfolioContract.StockTable.SYMBOL, s.getSymbol());
+        cv.put(PortfolioContract.StockTable.NAME, s.getName());
+        cv.put(PortfolioContract.StockTable.CURRENCY, s.getCurrency());
+        cv.put(PortfolioContract.StockTable.STOCK_EXCHANGE, s.getStockExchange());
+        cv.put(PortfolioContract.StockQuoteTable.PRICE, s.getQuote().getPrice().toString());
+
+        return cv;
+    }
+
+    public static Stock buildStock(Cursor c) {
+
+        Stock stock = null;
+        if (c.moveToFirst()) {
+            do {
+                String symbol = c.getString(c.getColumnIndex(PortfolioContract.StockTable.SYMBOL));
+                stock = new Stock(symbol);
+                stock.setName(c.getString(c.getColumnIndex(PortfolioContract.StockTable.NAME)));
+                stock.setCurrency(c.getString(c.getColumnIndex(PortfolioContract.StockTable.CURRENCY)));
+                stock.setStockExchange(c.getString(c.getColumnIndex(PortfolioContract.StockTable.STOCK_EXCHANGE)));
+
+                StockQuote quote = new StockQuote(symbol);
+                quote.setPrice(new BigDecimal(c.getString(c.getColumnIndex(PortfolioContract.StockQuoteTable.PRICE))));
+                stock.setQuote(quote);
+            } while (c.moveToNext());
+        }
+
+//        c.close();
+        return stock;
+    }
+}
