d8bbf8762e4c7921d474a6966e8017820f9619d6
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-10-20 18:49:31 +0300

    fetch real stock info

--- app/src/main/java/com/ilyarudyak/android/portfel/widget/StockWidgetService.java
+++ app/src/main/java/com/ilyarudyak/android/portfel/widget/StockWidgetService.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.ilyarudyak.android.portfel.R;
@@ -33,34 +34,44 @@ public class StockWidgetService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
 
+        // fetch stock info
+        fetchStock();
+
         // retrieve all of the widget ids: these are the widgets we need to update
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
         int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, StockWidgetProvider.class));
 
         for (int appWidgetId : appWidgetIds) {
-            mRemoteViews = new RemoteViews(getPackageName(), R.layout.widget_stock);
-            setRemoteViews();
-            buildIntent();
-            appWidgetManager.updateAppWidget(appWidgetId, mRemoteViews);
+            setRemoteViews(appWidgetManager, appWidgetId);
         }
     }
 
     // helper functions
+    private void setRemoteViews(AppWidgetManager appWidgetManager, int appWidgetId) {
+        mRemoteViews = new RemoteViews(getPackageName(), R.layout.widget_stock);
+        bindRemoteViews();
+        buildIntent();
+        appWidgetManager.updateAppWidget(appWidgetId, mRemoteViews);
+    }
     private void buildIntent() {
         Intent launchIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
         mRemoteViews.setOnClickPendingIntent(R.id.widget_stock_relative_layout, pendingIntent);
     }
-    private void getStock() {
+    private void fetchStock() {
         try {
             mStock = YahooFinance.get(mSymbol);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void setRemoteViews() {
+    private void bindRemoteViews() {
+
+        Log.d(TAG, "trying to set remote views");
+        Log.d(TAG, "stock==null " + (mStock == null));
 
         if (mStock != null) {
+            Log.d(TAG, "price=" + mStock.getQuote().getPrice());
             // set text fields
             mRemoteViews.setTextViewText(R.id.widget_stock_symbol, mStock.getSymbol() + " (" + mStock.getStockExchange() + ")");
             mRemoteViews.setTextViewText(R.id.widget_stock_price, mStock.getQuote().getPrice().toString());
