4c9aabc0a2a67043627fe99e1c0e57d66242daa0
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-04-07 22:37:10 +0300

    switch to gradle 2.0

--- app/src/main/java/com/ilyarudyak/android/portfel/ui/NewsDetailActivity.java
+++ app/src/main/java/com/ilyarudyak/android/portfel/ui/NewsDetailActivity.java
@@ -1,9 +1,10 @@
 package com.ilyarudyak.android.portfel.ui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.ilyarudyak.android.portfel.R;
 
@@ -11,21 +12,27 @@ public class NewsDetailActivity extends Activity {
 
     public static final String EXTRA_RSS_ITEM_URL_STRING =
             "com.ilyarudyak.android.portfel.ui.EXTRA_RSS_ITEM_URL_STRING";
+    private static final String TAG = NewsDetailActivity.class.getSimpleName();
 
-    private WebView mBrowser;
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
-        mBrowser = (WebView) findViewById(R.id.webkit);
+        mWebView = (WebView) findViewById(R.id.webkit);
+
+        // open redirected links in web view (not in browser)
+        mWebView.setWebViewClient(new WebViewClient());
 
         setWebView();
     }
 
     private void setWebView() {
-        Intent intent = getIntent();
-        String newsUrlStr = intent.getStringExtra(EXTRA_RSS_ITEM_URL_STRING);
-        mBrowser.loadUrl(newsUrlStr);
+        String newsUrlStr = getIntent().getStringExtra(EXTRA_RSS_ITEM_URL_STRING);
+        Log.d(TAG, newsUrlStr);
+        if (newsUrlStr != null) {
+            mWebView.loadUrl(newsUrlStr);
+        }
     }
 }
